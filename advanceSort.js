1ae4cc97fc8fb9d31c4d09c458954f561f9ccd68
Nat York <natyork@gmail.com>
2016-10-06 20:41:45 +0000

    cleaned up advanceSort.js

--- advanceSort.js
+++ advanceSort.js
@@ -30,25 +30,3 @@ students.sort(function(a,b){
 })
 
 console.log(students)
-
-
-// var items = [
-//   { name: 'Edward', value: 21 },
-//   { name: 'Sharpe', value: 37 },
-//   { name: 'And', value: 45 },
-//   { name: 'The', value: -12 },
-//   { name: 'Magnetic' },
-//   { name: 'Zeros', value: 37 }
-// ];
-// // sort by value
-// items.sort(function (a, b) {
-//   if (a.value > b.value) {
-//     return 1;
-//   }
-//   if (a.value < b.value) {
-//     return -1;
-//   }
-//   // a must be equal to b
-//   return 0;
-// });
-// // sort by name
